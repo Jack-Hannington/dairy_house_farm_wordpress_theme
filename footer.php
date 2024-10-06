d4905b0dc45929b7c764bebcefe3cf36f68e803e
Jack Hannington <hannington94@gmail.com>
2024-10-06 17:27:56 +0100

    footer and css updates

--- footer.php
+++ footer.php
@@ -2,34 +2,38 @@
 </div>
 <footer id="colophon" class="site-footer">
         <div class="container footer-links flex-column" style="width: 100%;">
-            <div class="row justify-content-between align-items-start">
+            <div class="row justify-content-center align-items-center text-center">
                 <!-- Logo or Site Title Column -->
-                <div class="col-lg-3 col-12 d-flex align-items-start flex-column">
+                <div class="col-12 d-flex justify-content-center flex-column">
                     <?php
                     if (function_exists('the_custom_logo')) {
                         $logo = get_custom_logo();
                         if ($logo) {
                             echo $logo;
                         } else {
-                            echo '<div id="siteTitle" class="siteTitle" style="color:white;">' . get_bloginfo('name') . '</div>';
+                            echo '<div id="siteTitle" class="siteTitle" style="color:white;font-weight:500;font-size:26px;">' . get_bloginfo('name') . '</div>';
 
                         }
                     }
                     ?>
-                    <div><p style="color:white;font-size:14px;margin-top:10px;">Dairy House Lane <br/>Altrincham <br/>WA14 5RE</p></div>
+                    <div class="mb-3 mt-2">
+                        <a href="https://maps.app.goo.gl/jVZrbnahNwtPosg59" style="color:white;margin-top:10px;">Dairy House Lane <br/>Altrincham <br/>WA14 5RE</a><br/>
+                        <a href="tel:07919114670" style="color:white;margin-top:10px;">Call us: 07919 114 670</a><br/>
+                        <a href="mailto:dairyhousefarmlivery@gmail.com" style="color:white;">Email us: dairyhousefarmlivery@gmail.com</a>
+                    </div>
                 </div>
                 <!-- Links Menu Column -->
-                <div class="col-lg-9 col-12 d-flex flex-column">
-                    <h6 class="footer-menu-title">Site links</h6>
+                <div class="col-12 d-flex flex-column text-center justify-content-center align-items-center mt-3">
+                    <h4 class="footer-menu-title mb-2">Site links</h4>
                     <?php wp_nav_menu(array(
                         'theme_location' => 'header-menu',
-                        'menu_class' => 'footer-menu d-flex4',
+                        'menu_class' => 'footer-menu d-flex max-450 text-center',
                         'fallback_cb' => false
                     )); ?>
                 </div>
             </div>
             <!-- Copyright Text -->
-            <div class="row">
+            <div class="row copyright">
                 <div class="col-12 text-center mt-3">
                     &copy; <?php echo date('Y'); ?> Dairy House Farm
                 </div>
